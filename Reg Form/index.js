1d397cad2c525b0ab5b90b53cba95d9b2ef7f611
kbir079 <ks400068@gmail.com>
2024-06-12 21:23:50 +0530

    Initial commit

---
+++ Reg Form/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(express.static("Public"))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect("mongodb://localhost:27017/Database")
+
+var db = mongoose.connection
+db.on("error",()=>{
+    console.log("Error in connecting to Database")
+})
+db.once("open",()=>{
+    console.log("Connected to Database")
+})
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var age = req.body.age;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var gender = req.body.gender;
+    var password = req.body.password;
+
+    var data = {
+        "name":name,
+        "age":age,
+        "email":email,
+        "phno":phno,
+        "gender":gender,
+        "password":password
+    }
+
+    db.collection("users").insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully")
+    })
+    return res.redirect("signup_successful.html")
+})
+
+app.get('/', (req, res) => {
+  res.set({
+    "Allow-acces-Allow-Origin":"*"
+  })
+  return res.redirect("index.html")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
